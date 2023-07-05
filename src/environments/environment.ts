e6af98cd97aaafea1f7c8eb77128ca0ae411b422
CeereMugambi <ceeremugambi@gmail.com>
2023-07-05 15:06:47 +0300

    Add new API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
-
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:4000',
+    
     weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13',
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
     XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: '5aede496e9msh7b401471318d3b1p11863ejsne6195ceb7a4d'
+    XRapidAPIKeyHeaderValue: 'f35cf2696bmsh631a4ffbf7dac0ep16e176jsnb79c380ddd79'
 };
