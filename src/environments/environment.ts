df5d8707718c9e7680761b6098706312564142f7
CeereMugambi <ceeremugambi@gmail.com>
2023-07-13 13:46:06 +0300

    Replace old API params with new API params

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:4000',
-    
-    weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13',
+    weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather',
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
     XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
     XRapidAPIKeyHeaderValue: 'f35cf2696bmsh631a4ffbf7dac0ep16e176jsnb79c380ddd79'
 };
