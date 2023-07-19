1b8a69ec6ea87be5c570114a02688909db9fecfd
CeereMugambi <ceeremugambi@gmail.com>
2023-07-19 16:57:43 +0300

    Update interface data

--- src/app/_models/weather.ts
+++ src/app/_models/weather.ts
@@ -1,9 +1,10 @@
 export interface IweatherData {
-  location: ILocation;
-  current: ICurrent;
+  location: iLocation;
+  current: iCurrent;
+  forecast: iForecast;
 }
 
-export interface ILocation {
+export interface iLocation {
   name: string;
   region: string;
   country: string;
@@ -14,13 +15,13 @@ export interface ILocation {
   localtime: string;
 }
 
-export interface ICurrent {
+export interface iCurrent {
   last_updated_epoch: number;
   last_updated: string;
   temp_c: number;
   temp_f: number;
   is_day: number;
-  condition: ICondition;
+  condition: iCondition;
   wind_mph: number;
   wind_kph: number;
   wind_degree: number;
@@ -40,7 +41,101 @@ export interface ICurrent {
   gust_kph: number;
 }
 
-export interface ICondition {
+export interface iCondition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+export interface iForecast {
+  forecastday: iForecastday[];
+}
+
+export interface iForecastday {
+  date: string;
+  date_epoch: number;
+  day: iDay;
+  astro: iAstro;
+  hour: iHour[];
+}
+
+export interface iDay {
+  maxtemp_c: number;
+  maxtemp_f: number;
+  mintemp_c: number;
+  mintemp_f: number;
+  avgtemp_c: number;
+  avgtemp_f: number;
+  maxwind_mph: number;
+  maxwind_kph: number;
+  totalprecip_mm: number;
+  totalprecip_in: number;
+  totalsnow_cm: number;
+  avgvis_km: number;
+  avgvis_miles: number;
+  avghumidity: number;
+  daily_will_it_rain: number;
+  daily_chance_of_rain: number;
+  daily_will_it_snow: number;
+  daily_chance_of_snow: number;
+  condition: iCondition2;
+  uv: number;
+}
+
+export interface iCondition2 {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+export interface iAstro {
+  sunrise: string;
+  sunset: string;
+  moonrise: string;
+  moonset: string;
+  moon_phase: string;
+  moon_illumination: string;
+  is_moon_up: number;
+  is_sun_up: number;
+}
+
+export interface iHour {
+  time_epoch: number;
+  time: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: iCondition3;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  will_it_rain: number;
+  chance_of_rain: number;
+  will_it_snow: number;
+  chance_of_snow: number;
+  vis_km: number;
+  vis_miles: number;
+  gust_mph: number;
+  gust_kph: number;
+  uv: number;
+}
+
+export interface iCondition3 {
   text: string;
   icon: string;
   code: number;
