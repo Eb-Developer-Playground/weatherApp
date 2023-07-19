9add95aeec6642a6e42975903049339624d0691a
CeereMugambi <ceeremugambi@gmail.com>
2023-07-19 10:49:18 +0300

    Add forecast Api params

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,20 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:4000',
+
     weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
-    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-    XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
-    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: '5aede496e9msh7b401471318d3b1p11863ejsne6195ceb7a4d'
+    weatherApiXRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    weatherApiXRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    weatherApiXRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    weatherApiXRapidAPIKeyHeaderValue: '5aede496e9msh7b401471318d3b1p11863ejsne6195ceb7a4d',
+
+    forecastApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    forecastApiXRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    forecastApiXRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+    forecastApiXRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    forecastApiXRapidAPIKeyHeaderValue: '5aede496e9msh7b401471318d3b1p11863ejsne6195ceb7a4d'
+
+
+
+
 };
