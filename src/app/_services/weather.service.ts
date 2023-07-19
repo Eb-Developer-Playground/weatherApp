6d9e744cd0b9c2ef842ed9d462c7673f83e4f6be
CeereMugambi <ceeremugambi@gmail.com>
2023-07-19 16:54:26 +0300

    Add Iforecast interface to service

--- src/app/_services/weather.service.ts
+++ src/app/_services/weather.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { IweatherData } from '@app/_models';
+import { IweatherData,iForecast } from '@app/_models';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +20,6 @@ export class WeatherService {
       .set('q', cityName)
       .set('units', 'metric')
       .set('mode', 'json')
-      
     })
   }
   }
